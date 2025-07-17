93b66399b1e8227125d6b8eb865b8188c02d7521
VikasWatore <watorevikas@gmail.com>
2025-07-17 11:53:17 +0530

    Introduction to javaScript

---
+++ 35_JavaScript/app.js
@@ -0,0 +1,3 @@
+console.log("Code is runnig");
+console.log("Code 2 is runnig");
+console.log("Code 3 is runnig");
---
+++ 35_JavaScript/script.js
@@ -0,0 +1,20 @@
+alert('Hello world');
+console.log("Code is runnig");
+console.log("Code 2 is runnig");
+console.log("Code 3 is runnig");
+
+var a=prompt("enter number");
+console.log(a);
+
+var istrue=confirm("Are you sure you want to leave this page");
+if(istrue){
+    console.log("you are fool....");
+    
+}else{
+    console.log("you are smart.....");
+    
+}
+
+
+document.title="JavaScript...";
+// document.body.style.backgroundColor="red";
