5aa3d94867cd2f2e98410c30b8a445bd9c8c4ecf
VikasWatore <watorevikas@gmail.com>
2025-07-19 10:32:03 +0530

    Variables Datatypes

---
+++ 36_JS_Varibles_Datatypes/app.js
@@ -0,0 +1,31 @@
+console.log("Welcome......");
+
+var a=51;
+var b=89;
+var c=78;
+var _d=89;
+// var 55v=55; // Not Allowed
+console.log(a+b+c);
+console.log(typeof a,typeof b,typeof c);
+
+const DOB=24122001;
+console.log(DOB);
+// DOB=DOB+1; not allowed because 
+// console.log(DOB);
+
+
+let x="Vikas";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof(x),typeof(y),typeof(z),typeof(p),typeof(q),typeof(r));
+
+let obj={
+    "name":"Vikas",
+    "Job Code":4500
+}
+console.log(obj);
