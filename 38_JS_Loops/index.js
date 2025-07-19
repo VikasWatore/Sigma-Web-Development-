4f600b83b505158d5b8adc9ee6e511ac277909d0
VikasWatore <watorevikas@gmail.com>
2025-07-19 14:03:45 +0530

    loops

---
+++ 38_JS_Loops/index.js
@@ -0,0 +1,38 @@
+console.log("tutorial on loops");
+
+let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Vikas",
+//     role: "Developer",
+//     Company: "Code_visu"
+// }
+// for (const key in obj) {
+//     console.log(key );
+// }
+
+// for (const element of "VIKAS") {
+//     console.log(element);
+    
+// }
+
+// let i=0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+    
+// }
+
+let i=0;
+do {
+    console.log(i);
+    i++;
+    
+} while (i<6);
