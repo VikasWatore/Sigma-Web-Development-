1fc7147439857dfafc56ca1ccecacadb12174586
VikasWatore <watorevikas@gmail.com>
2025-08-16 17:38:20 +0530

    Path Module

---
+++ 55_Backend_Filesystem/aboutPath.js
@@ -0,0 +1,8 @@
+import path from "path"
+let mypath=" c:\\Users\\vikas watore\\OneDrive\\Desktop\\SIGMA WebDevelopment\\55_Backend_Filesystem\\aboutPath.js"
+console.log(path.extname(mypath));
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+console.log(path.join("c:/","programm\\Vikas.txt"));
+
+
